360ec754519f9275cf38fe340771d00de2b0d966
K. Chandra Kiran <kiran83118311@gmail.com>
2026-04-24 12:13:36 +0530

    Practice on equality and comparision opeartors

--- 01_Core_Python/04_Opeartors.py
+++ 01_Core_Python/04_Opeartors.py
@@ -94,3 +94,105 @@ if mobile_number=="7013234455" or "9490151726" or "9390727285":
 print()
 time.sleep(2)
 print("end of application")
+##
+# Equality operators:In pythin equality opearators used to content comaparision and rturns boolean values.
+#Equality operators are == amd != (Equal to and not equal to).
+#Exampes:
+
+import time
+l1=[1,2,3,4,5,6]
+l2=[6,5,4,3,2,1]
+print(type(l1))
+print(type(l2))
+print()
+print(l1==l2)
+print()
+print(l1!=l2)
+print()
+print(type(l1==l2))
+print(type(l1!=l2))
+print()
+time.sleep(2)
+print("end of application")
+
+#Example2:
+
+import time
+str1="Python"
+str2="Full-stack python"
+obj1=str1==str2
+print(obj1)
+obj2=str1!=str2
+print(obj2)
+print(type(obj1))
+print(type(obj2))
+print()
+time.sleep(2)
+print("end of application")
+
+#Example3:
+
+import time
+age=24
+name="chandra kiran"
+if age==24 and name=="chandra kiran":
+    print("Eligible for voting.....")
+else:
+    print("Not eligible for voting......")
+print()
+time.sleep(2)
+print("end of application")
+
+#Example:
+
+import time
+chandu_age=25
+kiran_age=23
+print(chandu_age==kiran_age)
+print(type(chandu_age==kiran_age))
+print(chandu_age!=kiran_age)
+print(type(chandu_age!=kiran_age))
+print()
+time.sleep(2)
+print("end of application")
+
+#Comparision operators:Used to compare the variables or operands.
+#Comparisionoperators are < and > and <= and >= .
+
+#Examples:
+
+import time
+num1=120
+num2=240
+print(type(num1) and type(num2))
+print("==Comparision_opeartors===")
+print(num1<num2)
+print(type(num1<num2))
+print()
+print(num1>num2)
+print(type(num1>num2))
+print()
+print(num1<=num2)
+print()
+print(num1>=num2)
+print(type(num1>=num2))
+print()
+time.sleep(2)
+print("end of application")
+
+#Example:
+
+import time
+str1="kiran"
+str2="chandra"
+print(type(str1) and type(str2))
+print()
+print(str1<str2)
+print(str1>str2)
+print(str1<=str2)
+print(str1>=str2)
+print()
+print(type(str1<str2),type(str1>str2),type(str1<=str2),type(str1>=str2))
+print()
+time.sleep(2)
+print("end of application")
